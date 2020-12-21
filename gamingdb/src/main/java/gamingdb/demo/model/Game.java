9039e3504e550340388cd7aea216cdce21e22502
miklosattila <domestos1390@gmail.com>
2020-12-21 12:03:41 +0200

    nem bejelentkezessel kapcsoltos adatbazis kesz

--- gamingdb/src/main/java/gamingdb/demo/model/Game.java
+++ gamingdb/src/main/java/gamingdb/demo/model/Game.java
@@ -1,5 +1,90 @@
 package gamingdb.demo.model;
 
-public class Game {
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Games")
+
+public class Game {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="genre")
+	private String genre;
+	
+	@Column(name="realse date")
+	private String relaseDate;
+	
+	@OneToMany(mappedBy = "Publisher", cascade = CascadeType.ALL)
+	private Set<Publisher> publishers;
+	
+	@OneToMany(mappedBy = "Developer", cascade = CascadeType.ALL)
+	private Set<Developer> developers;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getRelaseDate() {
+		return relaseDate;
+	}
+
+	public void setRelaseDate(String relaseDate) {
+		this.relaseDate = relaseDate;
+	}
+
+	public Set<Publisher> getPublishers() {
+		return publishers;
+	}
+
+	public void setPublishers(Set<Publisher> publishers) {
+		this.publishers = publishers;
+	}
+
+	public Set<Developer> getDevelopers() {
+		return developers;
+	}
+
+	public void setDevelopers(Set<Developer> developers) {
+		this.developers = developers;
+	}
+	
 }
--- gamingdb/src/main/java/gamingdb/demo/model/Publisher.java
+++ gamingdb/src/main/java/gamingdb/demo/model/Publisher.java
@@ -1,5 +1,54 @@
 package gamingdb.demo.model;
 
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Publishers")
+
 public class Publisher {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="Type")
+	private String type;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 
 }
